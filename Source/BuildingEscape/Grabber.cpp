1d4b53b1756e93779728be87e41edfebe8d2b43e
GeekOnly <xs13@hotmail.co.th>
2020-03-28 10:18:44 +0700

    Raycasting Get name

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -40,7 +40,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		);
 
 	//Todo log out to test
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s. Position: %s"),*PlayerViewPointLocation.ToString(),*PlayerViewPointRotation.ToString());
+	//UE_LOG(LogTemp, Warning, TEXT("Location: %s. Position: %s"),*PlayerViewPointLocation.ToString(),*PlayerViewPointRotation.ToString());
 
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
 
@@ -54,5 +54,25 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		0.f,
 		10.f
 		);
+
+	//Setup query parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	//Line-trace (AKA ray-cast) out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+		);
+
+	//See what what we hit
+	AActor* ActorHit = Hit.GetActor();
+		if (ActorHit) 
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), *(ActorHit->GetName()));
+		}
 }
 
